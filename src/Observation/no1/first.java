c401ff34d2aebe92cec5f172b994a25691ec59d7
Erkanat <iamerkanat@gmail.com>
2022-10-06 15:37:05 +0600

    8

--- src/Observation/no1/first.java
+++ src/Observation/no1/first.java
@@ -3,5 +3,6 @@ package no1;
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello again!");
+        System.out.println("Maybe it was late hello");
     }
 }
