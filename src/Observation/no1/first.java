017d7bc90f3d4300da4c955c7104e3dc8db95a83
Erkanat <iamerkanat@gmail.com>
2022-10-06 15:42:11 +0600

    8

--- src/Observation/no1/first.java
+++ src/Observation/no1/first.java
@@ -4,5 +4,7 @@ public class first {
     public static void main(String[] args) {
         System.out.println("Hello again!");
         System.out.println("Maybe it was late hello");
+        System.out.println("Apologizes");
+        System.out.println("lalala");
     }
 }
